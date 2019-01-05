7790e0ce7cdab751743e1f535ac86357a9ab1d07
Lily George <lilyjgeorge@gmail.com>
2019-01-04 16:31:41 -0800

    Updates

---
+++ precache-manifest.21a811dc322b5ceca44b4d3412354ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcc36125fe41f08d02c0",
+    "url": "/rearrangeable-layout/static/js/runtime~main.dcc36125.js"
+  },
+  {
+    "revision": "f94742f58bf667284146",
+    "url": "/rearrangeable-layout/static/js/main.f94742f5.chunk.js"
+  },
+  {
+    "revision": "c59be5fcf60c1edff9b2",
+    "url": "/rearrangeable-layout/static/js/1.c59be5fc.chunk.js"
+  },
+  {
+    "revision": "f94742f58bf667284146",
+    "url": "/rearrangeable-layout/static/css/main.aa7c5cd9.chunk.css"
+  },
+  {
+    "revision": "488d636e196839259bee5946786e1516",
+    "url": "/rearrangeable-layout/index.html"
+  }
+];
